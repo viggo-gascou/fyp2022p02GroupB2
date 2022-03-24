14169f145c858c74aa81a4984376cb005c98c27f
Gustav Kristensen <gustavkrist@gmail.com>
2022-03-24 10:30:23 +0100

    Changed inner loop, ~33% faster

--- perimiter.py
+++ perimiter.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from PIL import Image
 from os import listdir
 from tqdm import tqdm
@@ -6,26 +7,15 @@ from math import pi
 
 
 def measure(seg_file):
-    area = 0
     perimiter = 0
-    img = Image.open(seg_file)
-    for x in range(img.size[0] - 1):
-        for y in range(img.size[1] - 1):
-            if img.getpixel((x, y)) != 0:
-                area += 1
-                surround = [
-                    (x, y + 1),
-                    (x, y - 1),
-                    (x + 1, y),
-                    (x - 1, y),
-                    (x - 1, y - 1),
-                    (x - 1, y + 1),
-                    (x + 1, y - 1),
-                    (x + 1, y + 1),
-                ]
-                surround = [img.getpixel(pix) for pix in surround]
-                if 0 in surround:
-                    perimiter += 1
+    img = np.array(Image.open(seg_file))
+    img[img != 0] = 1
+    for x1, x2 in zip(range(img.shape[0] - 1), range(3, img.shape[0] - 1)):
+        for y1, y2 in zip(range(img.shape[1] - 1), range(3, img.shape[1] - 1)):
+            view = img[x1:x2, y1:y2]
+            if view[1, 1] and np.sum(view) < 9:
+                perimiter += 1
+    area = np.sum(img)
     return (area, perimiter)
 
 
@@ -41,4 +31,4 @@ for seg in tqdm(seg_names):
 columns = {"img": img_names, "area": areas, "perimiter": perimiters}
 df = pd.DataFrame(columns)
 df["compactness"] = df["perimiter"] ** 2 / ((4 * pi) * df["area"])
-df.to_csv("data/area_perim.csv", index=False)
+df.to_csv("features/area_perim.csv", index=False)
