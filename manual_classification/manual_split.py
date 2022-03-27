3809617d95c97116a813f978c06f16b0beaf88d5
Gustav Kristensen <gustavkrist@gmail.com>
2022-03-27 12:48:39 +0200

    Reduced to the same 60 pictures

--- manual_classification/manual_split.py
+++ manual_classification/manual_split.py
@@ -7,13 +7,13 @@ img_names.sort()
 indices = [i for i in range(0, 451, 90)]
 indices = list(map(lambda x: x % 150, indices))
 
-buckets = []
-for i1, i2 in zip(indices, indices[1:]):
-    if i2 < i1:
-        bucket = img_names[i1:] + img_names[:i2]
-    else:
-        bucket = img_names[i1:i2]
-    buckets.append(bucket)
+buckets = [img_names[:60] for _ in range(5)]
+# for i1, i2 in zip(indices, indices[1:]):
+#     if i2 < i1:
+#         bucket = img_names[i1:] + img_names[:i2]
+#     else:
+#         bucket = img_names[i1:i2]
+#     buckets.append(bucket)
 
 try:
     rmtree("buckets")
