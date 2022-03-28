769a530e72600ec4c1b365cf1ce705c1a5d2715b
Gustav Kristensen <gustavkrist@gmail.com>
2022-03-28 11:28:04 +0200

    Multiprocessing resize, resized jpg training instead of png

--- measure_features.py
+++ measure_features.py
@@ -2,38 +2,43 @@ import pandas as pd
 import numpy as np
 from measure import measure
 from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
+
+
+def wrapper(tup):
+    return measure(*tup)
 
 
 # Paths to the directories with the images and corresponding segmentations to measure
-img_path = "resized_data/example_image_resized/"
-seg_path = "resized_data/example_segmentation_resized/"
-# Read the example ground thruth csv to get image ids and melanoma data
-df = pd.read_csv("data/example_ground_truth.csv")[["image_id", "melanoma"]]
+img_path = "resized_data/training_image_resized/"
+seg_path = "resized_data/training_segmentation_resized/"
 
-# Add paths and extensions to image ids
-img_files = [img_path + img + ".jpg" for img in df["image_id"]]
-seg_files = [seg_path + img + "_segmentation.png" for img in df["image_id"]]
+if __name__ == "__main__":
+    # Read the example ground thruth csv to get image ids and melanoma data
+    df = pd.read_csv("data/training_ground_truth.csv")[["image_id", "melanoma"]]
 
-print("Measuring features...")
-# Measure features using custom functions for each image and segmentation file
-# tqdm and list for the zip() facilitates a progress bar
-features = np.vstack(
-    [measure(img, seg) for img, seg in tqdm(list(zip(img_files, seg_files)))]
-)
-feature_names = [
-    "asymmetry",
-    "asymmetry_gauss",
-    "area",
-    "perimeter",
-    "compactness",
-    "color_dist",
-    "color_sd",
-    "color_score",
-]
-# Adding the columns of features to the data frame
-for col, feature in zip(features.T, feature_names):
-    df[feature] = col
-# Changing data type for the integer columns
-types = {k: int for k in ["area", "perimeter", "color_score"]}
-df.astype(types)
-df.to_csv("features/features.csv", index=False)
+    # Add paths and extensions to image ids
+    img_files = [img_path + img + ".jpg" for img in df["image_id"]]
+    seg_files = [seg_path + img + "_segmentation.png" for img in df["image_id"]]
+
+    print("Measuring features...")
+    # Measure features using custom functions for each image and segmentation file
+    # tqdm and list for the zip() facilitates a progress bar
+    features = np.vstack(process_map(wrapper, list(zip(img_files, seg_files)), chunksize=5))
+    feature_names = [
+        "asymmetry",
+        "asymmetry_gauss",
+        "area",
+        "perimeter",
+        "compactness",
+        "color_dist",
+        "color_sd",
+        "color_score",
+    ]
+    # Adding the columns of features to the data frame
+    for col, feature in zip(features.T, feature_names):
+        df[feature] = col
+    # Changing data type for the integer columns
+    types = {k: int for k in ["area", "perimeter", "color_score"]}
+    df.astype(types)
+    df.to_csv("features/features_training.csv", index=False)
--- resize.py
+++ resize.py
@@ -2,49 +2,46 @@ from os import listdir, mkdir
 from os.path import isdir
 from shutil import rmtree
 from PIL import Image
-from sys import argv
-from pathlib import Path
 from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
 
 
 """Resizes all images in all subdirectories of the directory provided as the
    command line argument when running the script."""
 
-# Checking if a directory was provided as an argument
-if len(argv) < 2:
-    print(
-        "Please provide a parent directory to resize the subdirectories of as an argument:"
-    )
-    print("python resize.py [path]")
-else:
-    data_dir = argv[1] + "/"
-    # Checking if provided path exists
-    if not Path(data_dir).exists():
-        print("Provided path does not exist")
-    elif not isdir(data_dir):
-        print("Provided path is not a directory")
-    else:
-        # Remove output directory if exists, to start from scratch
-        try:
-            rmtree("resized_data")
-        except FileNotFoundError:
-            pass
-        # Make output directory
-        mkdir("resized_data")
-        # List subdirectories of the provided parent directory
-        subdirs = listdir(data_dir)
-        subdirs = [dir for dir in subdirs if isdir(data_dir + dir)]
-        paths = list(map(lambda x: data_dir + x, subdirs))
-        # Go through each subdirectory and its path
-        for dir, path in zip(subdirs, paths):
-            # List the image names in the subdirectory
-            img_names = [file for file in listdir(path) if file[0] != "."]
-            # Make output subdirectory
-            resized_path = f"resized_data/{dir}_resized/"
-            mkdir(resized_path)
-            # Resize all images in the subdirectory and save them in the output
-            # subdirectory. tqdm() provides a progress bar.
-            for img_name in tqdm(img_names):
-                img = Image.open(path + "/" + img_name)
-                img.thumbnail((600, 600), resample=False)
-                img.save(resized_path + img_name)
+
+def resize(img_name, path, resized_path):
+    "Resizes image to 600 pixels in longest dimension"
+    img = Image.open(path + "/" + img_name)
+    img.thumbnail((600, 600), resample=False)
+    img.save(resized_path + img_name)
+
+
+def wrapper(tup):
+    """Wrapper for process_map"""
+    resize(*tup)
+
+
+data_dir = "data_to_resize/"
+if __name__ == "__main__":
+    # Remove output directory if exists, to start from scratch
+    try:
+        rmtree("resized_data")
+    except FileNotFoundError:
+        pass
+    # Make output directory
+    mkdir("resized_data")
+    # List subdirectories of the provided parent directory
+    subdirs = listdir(data_dir)
+    subdirs = [dir for dir in subdirs if isdir(data_dir + dir)]
+    paths = list(map(lambda x: data_dir + x, subdirs))
+    # Go through each subdirectory and its path
+    for dir, path in zip(subdirs, paths):
+        # List the image names in the subdirectory
+        img_names = [file for file in listdir(path) if file[0] != "."]
+        # Make output subdirectory
+        resized_path = f"resized_data/{dir}_resized/"
+        mkdir(resized_path)
+        # Resize all images in the subdirectory and save them in the output
+        # subdirectory. process_map provides a progress bar and multiprocesses.
+        process_map(wrapper, list(zip(img_names, [path] * len(img_names), [resized_path] * len(img_names))), chunksize=5)
