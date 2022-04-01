2757b9780b654ee79c9a452b295e08d7c387c18b
gustavkrist <gustavkrist@gmail.com>
2022-04-01 12:18:17 +0000

    to run on macbook

--- measure_features.py
+++ measure_features.py
@@ -9,12 +9,12 @@ def wrapper(tup):
 
 
 # Paths to the directories with the images and corresponding segmentations to measure
-img_path = "resized_data/training_image_resized/"
-seg_path = "resized_data/training_segmentation_resized/"
+img_path = "resized_data/example_image_resized/"
+seg_path = "resized_data/example_segmentation_resized/"
 
 if __name__ == "__main__":
     # Read the example ground thruth csv to get image ids and melanoma data
-    df = pd.read_csv("data/training_ground_truth.csv")[["image_id", "melanoma"]]
+    df = pd.read_csv("data/example_ground_truth.csv")[["image_id", "melanoma"]]
 
     # Add paths and extensions to image ids
     img_files = [img_path + img + ".jpg" for img in df["image_id"]]
@@ -43,7 +43,8 @@ if __name__ == "__main__":
     # Adding the columns of features to the data frame
     for col, feature in zip(features.T, feature_names):
         df[feature] = col
+    print(list(df))
     # Changing data type for the integer columns
     types = {k: int for k in ["area", "perimeter", "color_score"]}
     df.astype(types)
-    df.to_csv("features/features_train.csv", index=False)
+    df.to_csv("features/features_example.csv", index=False)
