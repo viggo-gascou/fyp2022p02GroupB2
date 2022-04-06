3971a6c99ad19889c44f9805f2349996b4d209f7
Gustav Kristensen <gustavkrist@gmail.com>
2022-04-06 22:22:32 +0200

    measured json data for training images

--- handin/measure_json.py
+++ handin/measure_json.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 from feature_functions import hist_json, percent_json
+from tqdm import tqdm
 
 df = pd.read_csv("../data/training_ground_truth.csv").drop(columns="seborrheic_keratosis")
-df = df.head(10)
 img_names = list(df["image_id"])
 features = {'pigment_network_percent': [], 'negative_network_percent': [], 'milia_like_cyst_percent': [], 'streaks_percent': [], 'pigment_network_hist': [], 'negative_network_hist': [], 'milia_like_cyst_hist': [], 'streaks_hist': []}
-for img in img_names:
+for img in tqdm(img_names):
     spmask = Image.open(f"../data/training_superpixels/{img}_superpixels.png")
     spmask.thumbnail((100, 100), resample=False)
     spmask = np.array(spmask)
@@ -20,3 +20,4 @@ for img in img_names:
 for feat, col in features.items():
     df[feat] = col
 df.to_csv("../features/json_train.csv", index=False)
+
